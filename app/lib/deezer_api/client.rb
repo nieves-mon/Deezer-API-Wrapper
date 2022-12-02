ebd2c61f272e8a1c42b6d566238a5bb936cf5c5a
nieves-mon <cunanan.nm@gmail.com>
2022-12-02 16:59:48 +0800

    Add chart endpoints

--- app/lib/deezer_api/client.rb
+++ app/lib/deezer_api/client.rb
@@ -1,3 +1,33 @@
 class DeezerApi::Client
     BASE_URL = "https://api.deezer.com"
+
+    def top_albums
+        response = connection.get("/chart/albums")
+        JSON.parse(response.body)
+    end
+
+    def top_tracks
+        response = connection.get("/chart/tracks")
+        JSON.parse(response.body)
+    end
+
+    def top_artists
+        response = connection.get("/chart/artists")
+        JSON.parse(response.body)
+    end
+
+    def top_playlists
+        response = connection.get("/chart/playlists")
+        JSON.parse(response.body)
+    end
+
+    def top_tracks
+        response = connection.get("/chart/podcasts")
+        JSON.parse(response.body)
+    end
+
+    private
+        def connection
+            @connection ||= Faraday.new(url: BASE_URL)
+        end
 end
