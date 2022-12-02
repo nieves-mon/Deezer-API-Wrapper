16e2bf279e0c128b74b98e255ac1e42bc7e8f64c
nieves-mon <cunanan.nm@gmail.com>
2022-12-02 20:13:35 +0800

    Generate top podcasts controller

---
+++ app/helpers/chart/top_podcasts_helper.rb
@@ -0,0 +1,2 @@
+module Chart::TopPodcastsHelper
+end
---
+++ test/controllers/chart/top_podcasts_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Chart::TopPodcastsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
